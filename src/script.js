636996c90c7196ab40876e0d8d76f2f112a077ea
akhil-rana <akhilrana0001@gmail.com>
2020-08-21 09:19:49 +0530

    projects Added

--- src/script.js
+++ src/script.js
@@ -24,6 +24,18 @@ $('#nextDownArrow svg').click(() => {
   goDownArrow();
 });
 
+$('#projects').click(() => {
+  if (position == 'about') {
+    goDownFromAbout();
+  }
+});
+
+$('#about').click(() => {
+  if (position == 'projects') {
+    goUpFromProjects();
+  }
+});
+
 document.querySelector('body').onwheel = movePage;
 document.onkeydown = movePage;
 
@@ -32,8 +44,14 @@ function movePage(event) {
   let keyPressed = event.keyCode || null;
   if (move > 0 || keyPressed === 40) {
     if (position == 'main') goDownArrow();
+    if (position == 'about') goDownFromAbout();
   } else if (move < 0 || keyPressed === 38) {
     if (position == 'about') goUpArrow();
+    if (position == 'projects') goUpFromProjects();
+  } else if (keyPressed === 37) {
+    if (position == 'projects') $('#previousProject svg').click();
+  } else if (keyPressed === 39) {
+    if (position == 'projects') $('#nextProject svg').click();
   }
   move = keyPressed = null;
 }
@@ -44,7 +62,7 @@ function goDownArrow() {
   if (deviceWidth <= 700) $('#navBar li').hide();
   if (deviceWidth > 700) $('#navBar li').show();
   $('#nextDownArrow').fadeOut();
-  $('.about').css('display', 'flex');
+  // $('.about').css('display', 'flex');
   anime({
     targets: '.container-typing',
     translateY: '-100%',
@@ -58,6 +76,7 @@ function goDownArrow() {
         direction: 'alternate',
         easing: 'easeInOutSine',
         complete: function () {
+          $('nav li span').removeClass('hover');
           $('#about').addClass('hover');
           $('body').css('overscroll-behavior', 'none');
           // $('.container-typing').hide();
@@ -102,6 +121,62 @@ function goUpArrow() {
   $('body').css('overscroll-behavior', 'auto');
 }
 
+function goDownFromAbout() {
+  position = 'processing';
+  $('#currentPosition').fadeOut(function () {
+    $(this).text('Projects').fadeIn();
+  });
+  anime({
+    targets: '.about',
+    translateY: 'calc(-200% - 6em)',
+    duration: 200,
+    easing: 'easeInOutQuad',
+    complete: function () {
+      $('#profileImage img').addClass('hover');
+    },
+  });
+  anime({
+    targets: '.projects',
+    translateY: '-200%',
+    duration: 900,
+    // delay: 50,
+    easing: 'easeOutElastic(1, 0.6)',
+    complete: function () {
+      position = 'projects';
+    },
+  });
+  $('nav li span').removeClass('hover');
+  $('#projects').addClass('hover');
+}
+
+function goUpFromProjects() {
+  position = 'processing';
+  $('#currentPosition').fadeOut(function () {
+    $(this).text('About').fadeIn();
+  });
+  anime({
+    targets: '.projects',
+    translateY: '100%',
+    duration: 400,
+    easing: 'easeInOutQuad',
+    complete: function () {
+      $('#profileImage img').removeClass('hover');
+    },
+  });
+  anime({
+    targets: '.about',
+    translateY: ['calc(-200% - 6em)', '-100%'],
+    duration: 1000,
+    // direction: 'reverse',
+    complete: function () {
+      position = 'about';
+
+    },
+  });
+  $('nav li span').removeClass('hover');
+  $('#about').addClass('hover');
+}
+
 $('#navBar #menuButton #menuIconSpan').click(() => {
   if (!navBarExpanded) {
     $('#navBar').animate({ height: '20em' }, 300, () => {
@@ -136,3 +211,115 @@ $('#navBar #menuButton #menuIconSpan').click(() => {
     });
   }
 });
+
+let currentProject = 1;
+let projectNames = [
+  'YT-Downloader-Backend',
+  'YT-Downloader',
+  'WebSight',
+  'WebSight-Backend',
+  'Google Search Scraper',
+  'D-Learn',
+  'Angular-to-do',
+];
+let projectDescriptions = [
+  'A nodeJS utility for downloading youtube videos using ytdl-core and fluent-ffmpeg with support for splitting and downloading chunk files from youtube servers for more speed.',
+  'The frontend for the (YT-Downloader-backend) made using basic HTML, jQuery, Bootstrap',
+  'Allowing visually and physically impaired individuals to perform certain web-tasks with ease without need of any personalized software/hardware all within a web browser. Used native web-speech API to perform all these tasks using only voice commands.',
+  'The nodeJS consolidated backend for WebSight which is responsible for scraping web sites like google.com, news.google.com, wikipedia etc. Used for google translate functionality too.',
+  'A really simple NodeJS project to scrape search results from google.com and return them in JSON format using Puppeteer and Cheerio.',
+  'A simple design based implementation for a distance learning android app. Implemented various features like course completion status, YouTube API support, WebView integration, Login/signup functionality, user profile update, password reset and Admin dashboard etc.',
+  'A simple frontend To-do list made using material-angular with dark theme support and ready for Heroku deployment.',
+];
+
+let projectsDeployStatus = [false, true, true, false, false, false, true];
+let projectsGithubLinks = [
+  'https://github.com/akhil-rana/YT-Downloader-Backend',
+  'https://github.com/akhil-rana/YT-Downloader',
+  'https://github.com/akhil-rana/WebSight',
+  'https://github.com/akhil-rana/WebSight-backend',
+  'https://github.com/akhil-rana/Google-Scrape-NodeJS',
+  'https://github.com/akhil-rana/D-Learn',
+  'https://github.com/akhil-rana/Angular-to-do',
+];
+let projectsDeployLinks = [ '',
+  'https://akhil-rana.github.io/YT-Downloader',
+  'https://web-sight.herokuapp.com', '', '', '',
+  'https://to-do-list-ang.herokuapp.com/',
+];
+
+$('#nextProject svg').click(() => {
+  if (currentProject < projectNames.length + 1) goToNextProject();
+});
+$('#previousProject svg').click(() => {
+  if (currentProject > 1) goToPreviousProject();
+});
+
+function goToNextProject(){
+  $('#nextProject svg, #previousProject svg').unbind('click');
+  if (currentProject == projectNames.length) {
+    $('#projectName').fadeOut(function () {
+      $(this).text('You might find some more on my github account :)').fadeIn();
+      $('#projectGithub').attr('href', 'https://github.com/akhil-rana/');
+      $('#projectDeployment').fadeOut();
+      $('#projectDescription').fadeOut();
+      $('#nextProject').fadeOut();
+      $('#previousProject svg').click(() => {
+        if (currentProject > 1) goToPreviousProject();
+      });
+      currentProject++;
+    });
+  } else {
+    $('#previousProject').fadeIn();
+    $('#projectName').fadeOut(function () {
+      $(this).text(projectNames[currentProject]).fadeIn();
+    });
+    $('#projectDescription').fadeOut(function () {
+      $(this).text(projectDescriptions[currentProject]).fadeIn();
+      if (projectsDeployStatus[currentProject])
+        $('#projectDeployment').fadeIn().attr('href', projectsDeployLinks[currentProject]);
+      else
+        $('#projectDeployment').fadeOut();
+      $('#projectGithub').attr('href', projectsGithubLinks[currentProject]);
+      currentProject++;
+      $('#nextProject svg').click(() => {
+        if (currentProject < projectNames.length + 1) goToNextProject();
+      });
+      $('#previousProject svg').click(() => {
+        if (currentProject > 1) goToPreviousProject();
+      });
+    });
+  }
+}
+
+function goToPreviousProject() {
+  $('#previousProject svg, #nextProject svg').unbind('click');
+  if (currentProject < 3) {
+    $('#previousProject').fadeOut();
+  }
+  $('#projectName').fadeOut(function () {
+    $(this)
+      .text(projectNames[currentProject - 2])
+      .fadeIn();
+  });
+  $('#projectDescription').fadeOut(function () {
+    if (projectsDeployStatus[currentProject - 2])
+      $('#projectDeployment')
+        .fadeIn()
+        .attr('href', projectsDeployLinks[currentProject - 2]);
+    else $('#projectDeployment').fadeOut();
+    $('#projectGithub').attr('href', projectsGithubLinks[currentProject - 2]);
+    $(this)
+      .text(projectDescriptions[currentProject - 2])
+      .fadeIn(function () {
+        currentProject--;
+        $('#previousProject svg').click(() => {
+          if (currentProject > 1) goToPreviousProject();
+        });
+        $('#nextProject svg').click(() => {
+          if (currentProject < projectNames.length + 1) goToNextProject();
+        });
+      });
+  });
+  $('#nextProject').fadeIn();
+}
--- src/utils/detectTouchSwipes.js
+++ src/utils/detectTouchSwipes.js
@@ -1,51 +1,54 @@
-document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchstart', handleTouchStart, false);
 document.addEventListener('touchmove', handleTouchMove, false);
 
-let xDown = null;                                                        
+let xDown = null;
 let yDown = null;
 
 function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
+  return (
+    evt.touches || evt.originalEvent.touches // browser API
+  ); // jQuery
+}
 
 function handleTouchStart(evt) {
-  const firstTouch = getTouches(evt)[0];                                      
-  xDown = firstTouch.clientX;                                      
-  yDown = firstTouch.clientY;                                      
-}                                                
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
 
 function handleTouchMove(evt) {
-  if ( ! xDown || ! yDown ) {
+  if (!xDown || !yDown) {
     return;
   }
 
-  let xUp = evt.touches[0].clientX;                                    
+  let xUp = evt.touches[0].clientX;
   let yUp = evt.touches[0].clientY;
 
   let xDiff = xDown - xUp;
   let yDiff = yDown - yUp;
 
-  if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-    if ( xDiff > 0 ) {
-      /* left swipe */ 
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) {
+      /* left swipe */
+      if (position == 'projects') $('#nextProject svg').click();
     } else {
       /* right swipe */
-    }                       
+      if (position == 'projects') $('#previousProject svg').click();
+    }
   } else {
-    if ( yDiff > 0 ) {
-      /* up swipe */ 
-      if (position == 'main') {
-        goDownArrow();
-      }
-    } else { 
+    if (yDiff > 0) {
+      /* up swipe */
+
+      if (position == 'main') goDownArrow();
+      if (position == 'about') goDownFromAbout();
+    } else {
       /* down swipe */
-      if (position == 'about') {
-        goUpArrow();
-      }
-    }                                                                 
+      if (position == 'about') goUpArrow();
+      if (position == 'projects') goUpFromProjects();
+    }
   }
   /* reset values */
   xDown = null;
-  yDown = null;                                             
+  yDown = null;
 }
