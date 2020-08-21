a6505b56caefb5c4f1e186c69e16f01a9557b3bc
akhil-rana <akhilrana0001@gmail.com>
2020-08-21 11:14:58 +0530

    subtle changes / improvements / responsive

--- src/script.js
+++ src/script.js
@@ -28,11 +28,21 @@ $('#projects').click(() => {
   if (position == 'about') {
     goDownFromAbout();
   }
+  if (navBarExpanded) {
+    setTimeout(() => {
+      $('#navBar #menuButton #menuIconSpan').click();
+    }, 400);
+  }
 });
 
 $('#about').click(() => {
   if (position == 'projects') {
     goUpFromProjects();
+  }
+  if (navBarExpanded) {
+    setTimeout(() => {
+      $('#navBar #menuButton #menuIconSpan').click();
+    }, 400);
   }
 });
 
@@ -221,6 +231,7 @@ let projectNames = [
   'Google Search Scraper',
   'D-Learn',
   'Angular-to-do',
+  'And this Website'
 ];
 let projectDescriptions = [
   'A nodeJS utility for downloading youtube videos using ytdl-core and fluent-ffmpeg with support for splitting and downloading chunk files from youtube servers for more speed.',
@@ -230,9 +241,10 @@ let projectDescriptions = [
   'A really simple NodeJS project to scrape search results from google.com and return them in JSON format using Puppeteer and Cheerio.',
   'A simple design based implementation for a distance learning android app. Implemented various features like course completion status, YouTube API support, WebView integration, Login/signup functionality, user profile update, password reset and Admin dashboard etc.',
   'A simple frontend To-do list made using material-angular with dark theme support and ready for Heroku deployment.',
+  'A simple yet beautiful personal portfolio website with dark theme support made without using any frontend libraries. Everything implemented with core CSS, JS, jQuery etc.'
 ];
 
-let projectsDeployStatus = [false, true, true, false, false, false, true];
+let projectsDeployStatus = [false, true, true, false, false, false, true, false];
 let projectsGithubLinks = [
   'https://github.com/akhil-rana/YT-Downloader-Backend',
   'https://github.com/akhil-rana/YT-Downloader',
@@ -241,11 +253,12 @@ let projectsGithubLinks = [
   'https://github.com/akhil-rana/Google-Scrape-NodeJS',
   'https://github.com/akhil-rana/D-Learn',
   'https://github.com/akhil-rana/Angular-to-do',
+  'https://github.com/akhil-rana/akhil-rana.github.io',
 ];
 let projectsDeployLinks = [ '',
   'https://akhil-rana.github.io/YT-Downloader',
   'https://web-sight.herokuapp.com', '', '', '',
-  'https://to-do-list-ang.herokuapp.com/',
+  'https://to-do-list-ang.herokuapp.com/', '',
 ];
 
 $('#nextProject svg').click(() => {
@@ -261,13 +274,15 @@ function goToNextProject(){
     $('#projectName').fadeOut(function () {
       $(this).text('You might find some more on my github account :)').fadeIn();
       $('#projectGithub').attr('href', 'https://github.com/akhil-rana/');
-      $('#projectDeployment').fadeOut();
-      $('#projectDescription').fadeOut();
-      $('#nextProject').fadeOut();
       $('#previousProject svg').click(() => {
         if (currentProject > 1) goToPreviousProject();
       });
       currentProject++;
+    });
+    $('#projectDeployment').fadeOut();
+    $('#nextProject').fadeOut();
+    $('#projectDescription').fadeOut(function(){
+      $(this).text('').fadeIn();
     });
   } else {
     $('#previousProject').fadeIn();
@@ -298,28 +313,22 @@ function goToPreviousProject() {
     $('#previousProject').fadeOut();
   }
   $('#projectName').fadeOut(function () {
-    $(this)
-      .text(projectNames[currentProject - 2])
-      .fadeIn();
+    $(this).text(projectNames[currentProject - 2]).fadeIn();
   });
   $('#projectDescription').fadeOut(function () {
     if (projectsDeployStatus[currentProject - 2])
-      $('#projectDeployment')
-        .fadeIn()
-        .attr('href', projectsDeployLinks[currentProject - 2]);
+      $('#projectDeployment').fadeIn().attr('href', projectsDeployLinks[currentProject - 2]);
     else $('#projectDeployment').fadeOut();
     $('#projectGithub').attr('href', projectsGithubLinks[currentProject - 2]);
-    $(this)
-      .text(projectDescriptions[currentProject - 2])
-      .fadeIn(function () {
-        currentProject--;
-        $('#previousProject svg').click(() => {
-          if (currentProject > 1) goToPreviousProject();
-        });
-        $('#nextProject svg').click(() => {
-          if (currentProject < projectNames.length + 1) goToNextProject();
-        });
+    $(this).text(projectDescriptions[currentProject - 2]).fadeIn(function () {
+      currentProject--;
+      $('#previousProject svg').click(() => {
+        if (currentProject > 1) goToPreviousProject();
       });
+      $('#nextProject svg').click(() => {
+        if (currentProject < projectNames.length + 1) goToNextProject();
+      });
+    });
   });
   $('#nextProject').fadeIn();
 }
